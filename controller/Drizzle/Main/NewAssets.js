12baa42e241aadb186b2b7958275e5adc6343178
rupeshilogsolution <rupesh.kumar@ilogsolution.com>
2023-08-29 16:58:19 +0530

    Done

--- controller/Drizzle/Main/NewAssets.js
+++ controller/Drizzle/Main/NewAssets.js
@@ -5,18 +5,15 @@ const os = require('os')
 const TotalNewAssets = async (req, res) => {
     const org = req.body.org;
     const type = req.body.type;
-      try {
+    try {
         await sql.connect(sqlConfig)
-        if(type.length>0){
-            console.log(`hlo`)
-
-            const result = await sql.query(`select sno,vendor,asset_tag,asset_name,serial_no,purchase_type,asset_type,asset_assign,location,asset_status,convert(varchar(15),purchase_date,121) as Assetdate from IPERISCOPE.dbo.tbl_new_assets with (nolock) where location = '${type}' order by asset_type ASC`)
+        if (type.length > 0) {
+            const result = await sql.query(`select sno,vendor,asset_tag,asset_name,serial_no,purchase_type,asset_type,manufacture,asset_assign,location,asset_status,convert(varchar(15),purchase_date,121) as Assetdate from IPERISCOPE.dbo.tbl_new_assets with (nolock) where location = '${type}' order by asset_type ASC`)
             res.status(200).send(result.recordset)
-        }else{
-            console.log(`select vendor,asset_tag,asset_name,serial_no,purchase_type,asset_type,asset_assign,location,asset_status,convert(varchar(15),purchase_date,121) as Assetdate from ${org}.dbo.tbl_new_assets with (nolock) order by asset_type ASC`)
-            const result = await sql.query(`select sno,vendor,asset_tag,asset_name,serial_no,purchase_type,asset_type,asset_assign,location,asset_status,convert(varchar(15),purchase_date,121) as Assetdate from ${org}.dbo.tbl_new_assets with (nolock) order by asset_type ASC `)
+        } else {
+            const result = await sql.query(`select sno,vendor,asset_tag,asset_name,serial_no,purchase_type,asset_type,manufacture,asset_assign,location,asset_status,convert(varchar(15),purchase_date,121) as Assetdate from ${org}.dbo.tbl_new_assets with (nolock) order by asset_type ASC `)
             res.status(200).send(result.recordset)
-        } 
+        }
     }
     catch (err) {
         res.send(err)
